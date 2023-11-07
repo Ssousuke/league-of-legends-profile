d5547a40dac4def39a8d9b058c053158294730e3
wesleysousukee@gmail.com <wesleysousukee@gmail.com>
2023-11-06 22:53:34 -0300

    chore: adicionado controller para consulta

--- LOLProfile.API.ZED/Controllers/LeagueController.cs
+++ LOLProfile.API.ZED/Controllers/LeagueController.cs
@@ -1,4 +1,5 @@
-﻿using LOLProfile.Application.Services.IServices;
+﻿using LOLProfile.API.ZED.RestService.IRestService;
+using LOLProfile.Application.Services.IServices;
 using LOLProfile.Domain.Interface;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -9,11 +10,20 @@ namespace LOLProfile.API.ZED.Controllers
     [ApiController]
     public class LeagueController : ControllerBase
     {
-        private readonly ILeagueService _serviceServices;
+        private readonly ILeagueRestServices _leagueRestServices;
 
-        public LeagueController(ILeagueService serviceServices)
+        public LeagueController(ILeagueRestServices leagueRestServices)
         {
-            _serviceServices = serviceServices;
+            _leagueRestServices = leagueRestServices;
+        }
+
+        [HttpGet("buscar-perfil/{summonerId}")]
+        public async Task<IActionResult> GetSummonerAsync(string summonerId)
+        {
+            var leagues = await _leagueRestServices.GetLeagueAsync(summonerId);
+            if (leagues is null)
+                return BadRequest("Não foi possível completar a requisição.");
+            return Ok(leagues);
         }
     }
 }
